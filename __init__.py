23c99607eee177624aa091bf811b1e0e4011151c
mishalp-crest <mishalp@splunk.com>
2024-05-08 12:57:05 +0530

    standard dev checks

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # File: __init__.py
 #
-# Copyright (c) OPSWAT, 2023
+# Copyright (c) OPSWAT, 2024
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
